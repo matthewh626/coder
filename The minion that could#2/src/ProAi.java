d64c052700010da8cc0399328bdc865b85f47ee1
matthewh626 <matthew.h626@gmail.com>
2016-01-29 15:05:31 +0000

    added varables and returns

--- The minion that could#2/src/ProAi.java
+++ The minion that could#2/src/ProAi.java
@@ -1,10 +1,19 @@
-public class ProAi {
+import java
+public class ProAi implements Runnable {
+Runnable r = new Runnable();
+boolean fleeing = false;
+boolean pathF = false;
 private int[] flee(){
-  
+  return {0,0};
 }
 private void think(){
   
 }
+
+Private int[] pathF(int,int){
+return {0,0};
+}
+
   Private void run(){
     
   }
